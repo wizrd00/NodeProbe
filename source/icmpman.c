5cc5b45af399272f87885e9640b892f3e6f09281
wizrd <wizrd.bin@proton.me>
2026-06-20 19:26:07 +0330

    fix two important mistakes

--- source/icmpman.c
+++ source/icmpman.c
@@ -13,7 +13,7 @@ uint16_t calc_icmpv4_checksum(icmpv4_echo_header_t *icmp)
 status_t check_echo_response(ethernet_header_t *res_eth, ethernet_header_t *req_eth, ipv4_header_t *res_ip, ipv4_header_t *req_ip, icmpv4_echo_header_t *res_icmp, icmpv4_echo_header_t *req_icmp)
 {
 	status_t _stat = SUCCESS;
-	bool match = (res_icmp->type == 0) || (res_icmp->code == 0) || (res_icmp->id == req_icmp->id);
+	bool match = (res_icmp->type == 0) && (res_icmp->code == 0) && (res_icmp->id == req_icmp->id);
 	if (!match)
 		_stat = FAILURE;
 	return _stat;
@@ -41,16 +41,17 @@ status_t icmpman_delete_context(icmpman_context_t *restrict context)
 	return _stat;
 }
 
-status_t icmpman_echo_request(icmpman_context_t *restrict context, uint32_t ip)
+status_t icmpman_echo_request(icmpman_context_t *restrict context)
 {
 	status_t _stat = SUCCESS;
 	size_t offset = 0;
 	unsigned char frame[FRAME_SIZE];
 	unsigned char *buffer = (unsigned char *) calloc(context->mtu_size, sizeof(unsigned char));
+	CHECK_NOTEQUAL(buffer, NULL, ERRALOC, "calloc() failed to allocate %zu bytes; %s", context->mtu_size, strerror(errno));
 	uint32_t src_ip = ((uint32_t) context->src_ip[0] | (uint32_t) context->src_ip[1] << 8 | (uint32_t) context->src_ip[2] << 16 | (uint32_t) context->src_ip[3] << 24);
-	CHECK_NOTEQUAL(buffer, NULL, ERRALOC, "calloc() failed to allocate %zu bytes from heap; %s", context->mtu_size, strerror(errno));
-	ethernet_header_t res_eth_header, req_eth_header = ETHERNET_DEFAULT_HEADER(context->src_mac, context->out_mac);
-	ipv4_header_t res_ip_header, req_ip_header = IPV4_DEFAULT_HEADER(context->id, FRAME_SIZE - sizeof(ethernet_header_t), src_ip, ip);
+	uint32_t dst_ip = ((uint32_t) context->dst_ip[0] | (uint32_t) context->dst_ip[1] << 8 | (uint32_t) context->dst_ip[2] << 16 | (uint32_t) context->dst_ip[3] << 24);
+	ethernet_header_t res_eth_header, req_eth_header = ETHERNET_DEFAULT_HEADER(context->src_mac, context->dst_mac);
+	ipv4_header_t res_ip_header, req_ip_header = IPV4_DEFAULT_HEADER(FRAME_SIZE - sizeof(ethernet_header_t), src_ip, dst_ip);
 	icmpv4_echo_header_t res_icmp_header, req_icmp_header = ICMPV4_ECHO_DEFAULT_HEADER(context->id);
 	struct sockaddr_ll req_addr = ICMPV4_ECHO_REQUEST_DEFAULT_ADDR();
 	struct pollfd pfd = {
@@ -81,11 +82,11 @@ status_t icmpman_echo_request(icmpman_context_t *restrict context, uint32_t ip)
 		CHECK_NOTEQUAL_FREE(recvfrom_ret, (ssize_t) -1, ERRRECV, buffer, "recvfrom() failed to receive ICMPv4 echo response on socket with fd = %d; %s", context->sockfd, strerror(errno));
 		CHECK_EQUAL_FREE((size_t) recvfrom_ret, FRAME_SIZE, ERRRECV, buffer, "size of the received frame is %zu instead of %zu", (size_t) recvfrom, FRAME_SIZE);
 		offset = 0;
-		memcpy((void *) &res_eth_header, (void *) frame, sizeof(ethernet_header_t));
+		memcpy((void *) &res_eth_header, (void *) buffer, sizeof(ethernet_header_t));
 		offset += sizeof(ethernet_header_t);
-		memcpy((void *) &res_ip_header, (void *) (frame + offset), sizeof(ipv4_header_t));
+		memcpy((void *) &res_ip_header, (void *) (buffer + offset), sizeof(ipv4_header_t));
 		offset += sizeof(ipv4_header_t);
-		memcpy((void *) &res_icmp_header, (void *) (frame + offset), sizeof(icmpv4_echo_header_t));
+		memcpy((void *) &res_icmp_header, (void *) (buffer + offset), sizeof(icmpv4_echo_header_t));
 		if (check_echo_response(&res_eth_header, &req_eth_header, &res_ip_header, &req_ip_header, &res_icmp_header, &req_icmp_header) == SUCCESS)
 			break;
 		LOGW("the received frame is invalid, trying again...");
